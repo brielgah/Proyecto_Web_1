2aba415c7b8abb1239b66d2829e07b06a7315be9
brielgah <gabrielgh@live.com.mx>
2020-06-18 21:56:01 -0500

    Correciones

--- Proyecto_01/inicioSesion.php
+++ Proyecto_01/inicioSesion.php
@@ -2,71 +2,82 @@
 //Parametros recibidos del formulario
 $user = $_POST["user"];
 $password = $_POST["password"];
-//Conectandose a la BD
-$conexion = mysqli_connect("localhost","root","57425595","usuarios");
-if(!$conexion)
+//Se checa que haya ingresado algo el usuario
+if(empty($user) && empty($password))
 {
-	mysqli_close($conexion);
 	echo "<script>
-			alert('Error al conectarse a la Base de Datos');
-			window.location='./index.html';
-			</script>";
+		alert('Debes ingresar un usuario y contraseña');
+		window.location='./inicioSesion.html';
+		</script>";
 }
 else
 {
-	//Haciendo la query
-	if($consulta = mysqli_prepare($conexion,"SELECT * FROM usuario where user=? and passwd=?"))
-	{	
-		mysqli_stmt_bind_param($consulta,'ss',$user,$password);
-		if(mysqli_stmt_execute($consulta))
-		{
-			mysqli_stmt_bind_result($consulta,$user,$password,$id);
-			mysqli_stmt_fetch($consulta);
-			//Existe el usuario
-			if(!mysqli_stmt_num_rows($consulta))
+	//Conectandose a la BD
+	$conexion = mysqli_connect("localhost","root","57425595","usuarios");
+	if(!$conexion)
+	{
+		mysqli_close($conexion);
+		echo "<script>
+				alert('Error al conectarse a la Base de Datos');
+				window.location='./index.html';
+				</script>";
+	}
+	else
+	{
+		//Haciendo la query
+		if($consulta = mysqli_prepare($conexion,"SELECT * FROM usuario where user=? and passwd=?"))
+		{	
+			mysqli_stmt_bind_param($consulta,'ss',$user,$password);
+			if(mysqli_stmt_execute($consulta))
 			{
-				if($id != 0)
-				{	
-					//El usuario es administrador
-					echo "administrador";
+				mysqli_stmt_bind_result($consulta,$usuario,$password,$id);
+				mysqli_stmt_fetch($consulta);
+				//Existe el usuario
+				if($usuario == $user)
+				{
+					if($id != 0)
+					{	
+						//El usuario es administrador
+						echo "administrador";
+					}
+					else
+					{
+						//El usuario es no es administrador
+						echo "usuario";
+
+					}
 				}
 				else
 				{
-					//El usuario es no es administrador
-					echo "usuario";
-
+					//No existe el usuario, regresa a pagina de inicio sesion
+					mysqli_stmt_close($conexion);
+					mysqli_close($conexion);
+					echo "<script>
+						alert('No existe el usuario o los datos ingresados son incorrectos');
+						window.location='./inicioSesion.html';
+						</script>";				
 				}
 			}
 			else
 			{
-				//No existe el usuario, regresa a pagina de inicio sesion
 				mysqli_stmt_close($conexion);
 				mysqli_close($conexion);
 				echo "<script>
-					alert('No existe el usuario o los datos ingresados son incorrectos');
-					window.location='./inicioSesion.html';
-					</script>";				
+					alert('Hubo un error al realizar la consulta');
+					window.location='./index.html';
+					</script>";	
 			}
 		}
 		else
 		{
+			//Hubo un error
 			mysqli_stmt_close($conexion);
 			mysqli_close($conexion);
 			echo "<script>
 				alert('Hubo un error al realizar la consulta');
 				window.location='./index.html';
-				</script>";	
+				</script>";				
 		}
-	}
-	else
-	{
-		//Hubo un error
-		mysqli_stmt_close($conexion);
-		mysqli_close($conexion);
-		echo "<script>
-			alert('Hubo un error al realizar la consulta');
-			window.location='./index.html';
-			</script>";				
 	}
 }
 ?>
