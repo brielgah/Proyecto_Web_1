8d528a2699fabd59b9ef16d33ce56d5e6d0fec92
brielgah <gabrielgh@live.com.mx>
2020-06-18 21:41:46 -0500

    Correcciones

--- Proyecto_01/inicioSesion.php
+++ Proyecto_01/inicioSesion.php
@@ -3,15 +3,6 @@
 $user = $_POST["user"];
 $password = $_POST["password"];
 //Conectandose a la BD
-if(empty($user))
-{
-	echo "entra";
-	echo "<script>
-		alert('Debes ingresar un usuario y contraseña');
-		window.location='./inicioSesion.html';
-		</script>";	
-}
-
 $conexion = mysqli_connect("localhost","root","57425595","usuarios");
 if(!$conexion)
 {
@@ -27,13 +18,12 @@ else
 	if($consulta = mysqli_prepare($conexion,"SELECT * FROM usuario where user=? and passwd=?"))
 	{	
 		mysqli_stmt_bind_param($consulta,'ss',$user,$password);
-<<<<<<< HEAD
 		if(mysqli_stmt_execute($consulta))
 		{
-			mysqli_stmt_bind_result($consulta,$usuario,$contraseña,$id);
-			mysqli_stmt_fetch($consulta);			
+			mysqli_stmt_bind_result($consulta,$user,$password,$id);
+			mysqli_stmt_fetch($consulta);
 			//Existe el usuario
-			if($usuario == $user)
+			if(!mysqli_stmt_num_rows($consulta))
 			{
 				if($id != 0)
 				{	
@@ -53,42 +43,17 @@ else
 				mysqli_stmt_close($conexion);
 				mysqli_close($conexion);
 				echo "<script>
-					alert('El usuario o la contraseña son incorrectos');
+					alert('No existe el usuario o los datos ingresados son incorrectos');
 					window.location='./inicioSesion.html';
-					</script>";			
-=======
-		mysqli_stmt_execute($consulta);
-		if(mysqli_stmt_bind_result($consulta,$user,$password,$id))
-		{
-			mysqli_stmt_fetch($consulta);
-			//Existe el usuario
-			if($id != 0)
-			{
-				//El usuario es administrador
-				header("");
-			}
-			else
-			{
-				//El usuario es no es administrador
-				header("");
-
->>>>>>> 80fc4df7c94730bc0838af9419336228fc42afa1
+					</script>";				
 			}
 		}
 		else
 		{
-<<<<<<< HEAD
 			mysqli_stmt_close($conexion);
 			mysqli_close($conexion);
 			echo "<script>
 				alert('Hubo un error al realizar la consulta');
-=======
-			//No existe el usuario, regresa a pagina de inicio
-			mysqli_stmt_close($conexion);
-			mysqli_close($conexion);
-			echo "<script>
-				alert('El usuario ingresado no existe,verifica tus datos');
->>>>>>> 80fc4df7c94730bc0838af9419336228fc42afa1
 				window.location='./index.html';
 				</script>";	
 		}
@@ -104,9 +69,4 @@ else
 			</script>";				
 	}
 }
-<<<<<<< HEAD
 ?>
-=======
-?>
-
->>>>>>> 80fc4df7c94730bc0838af9419336228fc42afa1
