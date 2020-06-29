7344188da6ca1e4d16a4cfeb87dc7abb95df99ff
brielgah <gabrielgh@live.com.mx>
2020-06-29 03:34:11 -0500

    mamadas

---
+++ Proyecto_01/genPdf.php
@@ -0,0 +1,56 @@
+<?php
+require_once('./LatexTemplate.php');
+// Conexión con la BD, la info del alumno debe ser guardada en data
+/*
+
+try{
+    LatexTemplate::download($data,'template.tex',$data['boleta'] . ".pdf");
+}catch(Exception $e){
+    echo $e -> getMessage();
+}*/
+session_start();
+$boleta=$_SESSION["password"];
+$query1="SELECT * FROM datos WHERE boleta=?";
+$conexion=mysqli_connect("localhost","root","57425595","alumnos");
+if(!$conexion)
+{
+	echo "<script>
+			alert('Error al conectarse a la Base de Datos');
+			window.location='./index.html';
+			</script>";
+}
+else
+{
+	if($consulta=mysqli_prepare($conexion,$query1))
+    {
+        mysqli_stmt_bind_param($consulta,'s',$boleta);
+        if(mysqli_stmt_execute($consulta))
+        {
+            mysqli_stmt_bind_result($consulta,$nombre,$ap_paterno,$ap_materno,$escuela,$estado,$direccion,$municipio,$cp,$email,$fechaNacimiento,$telefono,$celular,$promedio,$curp,$boleta);
+            mysqli_stmt_fetch($consulta);
+        }
+        else
+        {
+            mysqli_stmtm_close($consulta);
+            mysqli_close($conexion);
+            echo "<script>
+                alert('Hubo un error al realizar la consulta');
+                window.location='./index.html';
+                </script>"; 
+        }
+    }
+}
+$nombreCompleto = $ap_paterno." ".$ap_materno." ".$nombre;
+$direccionCompleta= $direccion." ".$municipio." CP:".$cp;
+$data = array(
+		'nombre' => $nombreCompleto,
+		'boleta' => $boleta,
+		'direccion' => $direccionCompleta,
+		'escproc' => $escuela
+);
+try {
+	LatexTemplate::download($data, 'genPdf.tex', 'ejemplo.pdf');
+} catch(Exception $e) {
+	echo $e -> getMessage();
+}
+?>
